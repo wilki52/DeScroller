64069c4954fe49d79dd1b130e07d989befe06269
wilki52 <williamk3210@gmail.com>
2024-08-29 17:54:41 -0700

    Organize files

--- background.js
+++
@@ -1,4 +0,0 @@
-let blocked_sites = ["reddit.com/r/singedmains"]
-console.log("background check!");
-
-
---
+++ src/popup.js
@@ -0,0 +1,7 @@
+console.log("This is a popup!");
+
+
+var button = document.querySelector(".logo");
+button.addEventListener("click", function(){
+    alert('rebbit');
+});
